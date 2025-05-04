ed7a0a02c24fb3e118e033f103d82643f322dcc6
Ayub40 <ayubk4028@gmail.com>
2025-05-05 01:42:24 +0600

    Conditional types

--- module2/2.9.ts
+++ module2/2.9.ts
@@ -1,3 +1,41 @@
 {
     // 2-9: Conditional types
+
+    // Example 1
+    type a1 = null;
+    type b1 = undefined;
+
+    type x = a1 extends null ? true : false;  // conditional type
+
+    // Example 2
+    type a2 = number;
+    type b2 = undefined;
+
+    type y = a1 extends null ? true : false;
+
+    // Example 3
+    type a3 = null;
+    type b3 = undefined;
+
+    type z = a1 extends null ? true : b1 extends undefined ? undefined : any;
+
+    //---------------------------------------------------------------------------
+    //---------------------------------------------------------------------------
+    //---------------------------------------------------------------------------
+
+    type Sheikh = {
+        bike: string;
+        car: string;
+        ship: string;
+        plane: string;
+    }
+
+    // car ase kina / bike ase kina / ship ase kina / tractor ase kina
+    type CheckVehicle<T> = T extends keyof Sheikh ? true : false
+
+    // type CheckVehicle<T> = T extends "bike" | "car" | "ship" ? true : false
+
+    // type HasTractor = CheckVehicle<"tractor">
+    type HasPlane = CheckVehicle<"plane">
+
 }
