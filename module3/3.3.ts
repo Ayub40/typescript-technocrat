e4ec484ef6f536a726db8cd29fd4467517e50b29
Ayub40 <ayubk4028@gmail.com>
2025-05-07 00:51:38 +0600

    type guard 3.3 and 3.4

--- module3/3.3.ts
+++ module3/3.3.ts
@@ -1,3 +1,54 @@
 {
     // 3-3: Type guard using typeof & in
+
+    type Alphanumeric = string | number;
+
+    //(typeof param1 === "string" && typeof param2 === "string")
+    const add = (param1: Alphanumeric, param2: Alphanumeric): Alphanumeric => {
+        if (typeof param1 === "number" && typeof param2 === "number") {
+            return param1 + param2;
+        } else {
+            return param1.toString() + param2.toString();
+        }
+        // else {
+        //     throw new Error("Invalid input types");
+        // }
+    };
+
+    const result1 = add('2', '3');
+    console.log(result1);
+
+    // console.log(add(1, 2)); // 3
+    // console.log(add("Hello", "World")); // HelloWorld
+
+    // in guard
+    type NormalUser = {
+        name: string;
+    };
+
+    type AdminUser = {
+        name: string;
+        role: 'admin';
+    };
+
+    const getUser = (user: NormalUser | AdminUser) => {
+        if ('role' in user) {
+            console.log(`My name is ${user.name} and my role is ${user.role}`);
+        } else {
+            console.log(`My name is ${user.name}`);
+        }
+    };
+
+    const normalUser: NormalUser = {
+        name: "Mr. Normal Bhai",
+        // role: "admin",
+    };
+
+    const adminUser: AdminUser = {
+        name: "Mr. Admin Bhai",
+        role: "admin",
+    }
+
+    getUser(adminUser);
+
 }
